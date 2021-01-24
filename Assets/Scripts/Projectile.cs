e1e9d4dd831693f2f73ef9a540931d2e9554ff75
Pavel Pek <pekpavel96@gmail.com>
2021-01-24 17:26:36 +0100

    Moving projectile
    - Added movement to the projectile via Projectile script.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * (speed * Time.deltaTime));       
+    }
+}
