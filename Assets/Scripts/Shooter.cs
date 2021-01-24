d6a4b95f219ca9d1a7dde37a6546a34bd7ba3e80
Pavel Pek <pekpavel96@gmail.com>
2021-01-24 17:17:16 +0100

    Cactus projectile instantiation
    - Spawning cactus projectile during attack animation

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject gun;
+    
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
