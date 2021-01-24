ec07708dbaf74242e76713e0106afda4dbfbee99
Pavel Pek <pekpavel96@gmail.com>
2021-01-24 17:47:21 +0100

    Added health & damage mechanism
    - Added health & damage mechanism when the projectile hits attacker, it reduces its health

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
+    [SerializeField] private float damage = 50f;
     
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * (speed * Time.deltaTime));       
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DealDamage(damage);
     }
 }
