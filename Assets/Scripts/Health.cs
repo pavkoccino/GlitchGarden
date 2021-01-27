c7d1b2e1fdb80605d203690d8cf7f19f5a74de35
Pavel Pek <pekpavel96@gmail.com>
2021-01-27 09:49:33 +0100

    Added death particle effect
    - Death particle effect triggers when attacker dies.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float health = 100f;
+    [SerializeField] private GameObject deathVFX;
 
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX) return;
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        Destroy(deathVFXObject, 1f);
     }
 }
