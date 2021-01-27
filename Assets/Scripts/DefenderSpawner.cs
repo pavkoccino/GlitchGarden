e8ae5af2cd6153dcc9d532f63f2f400ae6ddb654
Pavel Pek <pekpavel96@gmail.com>
2021-01-27 10:47:17 +0100

    Defenders now snap exactly to squares
    - Defenders now snap exactly to squares

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -10,11 +10,27 @@ public class DefenderSpawner : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("Mouse clicked");
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
     }
 
-    private void SpawnDefender()
+    private Vector2 GetSquareClicked()
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 gridPos)
+    {
+        GameObject newDefender = Instantiate(defender, gridPos, Quaternion.identity) as GameObject;
+        Debug.Log(gridPos);
     }
 }
