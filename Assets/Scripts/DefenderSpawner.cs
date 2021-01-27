082773daf5008c92f82c9e620ecf2c1b4cc5125e
Pavel Pek <pekpavel96@gmail.com>
2021-01-27 09:59:11 +0100

    Added defender spawner
    - Defender is now spawned on click.

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject defender;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse clicked");
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+    }
+}
